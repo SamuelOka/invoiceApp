cfe1ff3966d7f545c22ec026897cb872012dfbd8
SamuelOka <126283127+SamuelOka@users.noreply.github.com>
2026-04-23 08:24:49 +0100

    Update invoiceDetails.jsx

--- src/invoiceDetails.jsx
+++ src/invoiceDetails.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { useInvoices } from "./invoiceContext.jsx";
-import EditInvoiceDrawer from "./EditInvoiceDrawer.jsx";
+import EditInvoiceDrawer from "./EditInvoiceDrawer";
 
 function fmtDate(dateStr) {
   return new Date(dateStr).toLocaleDateString("en-GB", {
